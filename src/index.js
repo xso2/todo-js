161396a57ecefa183bf87a13eae77f90fbf2ab33
Gren8989! <odaken1002@gmail.com>
2022-08-18 07:25:29 +0000

    Update 3 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //div生成
+  const div = document.createElement("div");
+  console.log(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
